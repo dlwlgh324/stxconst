eb6c203208eba7cbe06e1b6ccbbbc13853e7f329
lee jiho <89637792+dlwlgh324@users.noreply.github.com>
2021-11-23 01:26:24 +0900

    메뉴 수정, 효과 적용
    메뉴 수정, 효과 적용

--- js/script.js
+++ js/script.js
@@ -69,4 +69,29 @@ $(document).ready(function () {
         });
     });
 
+    // 부드럽게 위치 이동하는 코드
+    var moveEl = $('.move');
+    // 클릭시 해당위치로 부드럽게 이동한다.
+    $.each(moveEl, function () {
+
+        $(this).click(function (e) {
+            // href 를 일단 막아서 위치이동 못하게함.
+            e.preventDefault();
+            // 가야하는 곳의 위치를 파악한다.
+            var tg = $(this).attr('href');
+            var num;
+            // 혹시 #아이디 가 없다면
+            if (tg == '#') {
+                num = 0;
+            } else {
+                // 어느 만큼 이동해야 하는 지를 숫자로 파악한다.
+                num = $(tg).offset().top;
+            }
+
+            // 움직이자
+            $('html, body').stop().animate({
+                scrollTop: num
+            }, 400);
+        });
+    });
 });
